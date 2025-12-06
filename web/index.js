e63bd16f533eff1dfc49732092bd4977e2e0ec9a
900 <163431447+mua900@users.noreply.github.com>
2025-12-06 23:14:59 +0300

    fix loading of the wasm module

--- web/index.js
+++ web/index.js
@@ -1,4 +1,23 @@
-async function load_soundtoy() {
+// evaluator type
+const BYTECODE_INTERP = 0;
+const TREE_INTERP = 1;
+
+// soundtoy functions
+let st_initialize;
+let st_sampler_create;
+let st_sampler_destroy;
+let st_check_expression_string;
+let st_sampler_set_expression;
+let st_sampler_evaluate;
+let st_sampler_step_time;
+let st_sampler_set_sample_rate;
+
+let soundtoy_ready;  // promise
+let resolve_soundtoy_ready;
+
+soundtoy_ready = new Promise((resolve) => {resolve_soundtoy_ready = resolve;})
+
+Module.onRuntimeInitialized = function load_soundtoy() {
 	st_initialize = Module.cwrap("st_initialize", "boolean", []);
 	st_sampler_create = Module.cwrap("st_sampler_create", "number", ["number", "number"]);
 	st_sampler_destroy = Module.cwrap("st_sampler_destroy", null, ["number"]);
@@ -7,10 +26,47 @@ async function load_soundtoy() {
 	st_sampler_evaluate = Module.cwrap("st_sampler_evaluate", "number", ["number"]);
 	st_sampler_step_time = Module.cwrap("st_sampler_step_time", null, ["number", "number"]);
 	st_sampler_set_sample_rate = Module.cwrap("st_sampler_set_sample_rate", null , ["number", "number"]);
+
+	resolve_soundtoy_ready();
 }
 
-load_soundtoy();
+let sampler_left;
+let sampler_right;
 
-function say_hello() {
-	console.log("Hello");
+async function create_samplers () {
+	await soundtoy_ready;
+
+	console.log("Loaded soundtoy library");
+
+	if (!st_initialize()) {
+		console.error("Failed to initialize soundtoy library");
+		return;
+	}
+
+	const sample_rate_default = 48000;
+
+	sampler_left = st_sampler_create(TREE_INTERP, sample_rate_default);
+	if (!sampler_left) {
+		console.error("Failed to create a sampler");
+	}
+
+	sampler_right = st_sampler_create(TREE_INTERP, sample_rate_default);
 }
+
+// ui elements
+let button;
+let expression_input;
+
+function setup_ui() {
+	button = document.getElementById("HelloButton");
+	expression_input = document.getElementById("ExpressionInput");
+
+	button.addEventListener("click", () => { console.log("Hello"); });
+}
+
+function main() {
+	create_samplers();
+	setup_ui();
+}
+
+main();
